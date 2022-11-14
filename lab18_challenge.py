ed902b1b1f86519e81092b0ba64b7b8d05816abd
maria <mariaquinatnamsis@gmail.com>
2022-11-14 22:24:27 +0000

    complete lab18 challenge

---
+++ lab18_challenge.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+def main():
+    username = input("Enter your name\n>")
+
+    weekday = input("Enter the day of week\n>")
+
+    print("Hello, " + username +"!\nHappy " + weekday +  "!" )
+
+main()
