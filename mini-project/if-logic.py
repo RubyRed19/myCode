c9dfb06df8ee890bf3b337b15062c40c5e51d85b
Maria <mariaquintanamsis@gmail.com>
2022-11-28 04:08:56 +0000

    mixed lists

--- mini-project/if-logic.py
+++ mini-project/if-logic.py
@@ -1,37 +1,45 @@
 #!/usr/bin/env python3
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+        
 
-def main():
-    user_score = 0;
-    add_points = 20;
-    play_again = true;
-
-    human_anatomy = [
-            "What is the largest organ in the body?",
-            "Where are the smallest muscles in the body found?",
-            "Which is the only organ capable of regeneration?",
-            "What does the orbital cavity contain?",
-            "Which blood type is considered the universal donor?"
+questions_prompt = [
+            "What is the largest organ in the body? \n(a) heart \n(b) stomach \n(c) intestines \n(d) skin\n->",
+            "Where are the smallest muscles in the body found? \n(a) eyes \n(b) ears \n(c) toes \n(d) nose\n-> ",
+            "Which is the only organ capable of regeneration? \n(a) kidney \n(b) liver \n(c) spleen \n(d) heart\n->",
+            "What does the orbital cavity contain? \n(a) eyes \n(b) teeth \n(c) intestine \n(d) stomach\n->",
+            "Which is the longer of the two bones in the forearm? \n(a) ulna \n(b) humerous \n(c) radius \n(d) femur\n->"
             ]
 
-    american_history = [];
+questions = [
+          Question(questions_prompt[0], "d"),
+          Question(questions_prompt[1], "b"),
+          Question(questions_prompt[2], "b"),
+          Question(questions_prompt[3], "a"),
+          Question(questions_prompt[4], "a"),
+        ];
 
-    user_name = input("Welcome to the Quiz-Me-Experience!  Please enter a username.\n-->").title()
-    print(f"{user_name}, It's now time to pick a 'Quiz-Me' Category.")
+user_name = input("Welcome to the Quiz-Me-Experience!  Please enter a username.\n-->").title()
+print(f"{user_name}, it's now time to pick a 'Quiz-Me' Category.")
 
-    topic_choice = input("\nYour quiz will have 5 questions, you must get 3 correct to pass.\nSelect 1 for Human Anatomy, select 2 for American History.\n-->")
+topic_choice = input("\nYour quiz will have 5 questions on human anatomy. Press any key to continue.\n-->")
 
-while play_again == true:
+def run_quiz(questions):
+    user_score = 0
+    play_again = True
 
-    if topic_choice == "1":
-        print(f"Question 1: {human_anatomy[0]}")
-        answer_one = input("\n-->").lower()
-        if "skin" in answer_one:
-            user_score += add_points 
-            print("Correct!")
-            print(f"\n {user_name}, your current score is {user_score}")
-        else:
-            print("Sorry, the correct answer was: skin");
-
-
-
-main()
+    for question in questions:
+        while play_again == True:
+            answer = input(question.prompt)
+            if answer == question.answer:
+                user_score += 20
+                print(f"Correct! {user_name} your score is {user_score}")
+                break
+            elif answer == "" or answer not in ["a", "b", "c", "d"]:
+                print("You must select one of the answer choices!")
+            elif answer != question.answer:
+                print(f"Sorry, the correct answer was {question.answer}")
+                break
+run_quiz(questions)
